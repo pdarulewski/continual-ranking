6a9136f89e3026da26fd6818a20cd049016a207c
Pawel Darulewski <pawel.darulewski@gmail.com>
2022-02-02 10:51:47 +0100

    feat: project init and paths

---
+++ continual_learning/config/paths.py
@@ -0,0 +1,9 @@
+import os
+from pathlib import Path
+
+ROOT_DIR = os.path.abspath(os.path.join(os.path.dirname(__file__), os.path.pardir, os.path.pardir))
+
+LOG_DIR = os.path.join(ROOT_DIR, 'log')
+Path(LOG_DIR).mkdir(parents=True, exist_ok=True)
+
+WANDB_DIR = os.path.join(ROOT_DIR, 'wandb')
