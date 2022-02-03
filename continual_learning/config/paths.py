698239c42a9d9314377bafcc5247d87f0d1affe3
Pawel Darulewski <pawel.darulewski@gmail.com>
2022-02-03 13:51:52 +0100

    feat: path for checkpoints

--- continual_learning/config/paths.py
+++ continual_learning/config/paths.py
@@ -4,6 +4,5 @@ from pathlib import Path
 ROOT_DIR = os.path.abspath(os.path.join(os.path.dirname(__file__), os.path.pardir, os.path.pardir))
 DATA_DIR = os.path.join(ROOT_DIR, 'data')
 LOG_DIR = os.path.join(ROOT_DIR, 'log')
+CHECKPOINT_PATH = os.path.join(ROOT_DIR, 'checkpoints')
 Path(LOG_DIR).mkdir(parents=True, exist_ok=True)
-
-WANDB_DIR = os.path.join(ROOT_DIR, 'wandb')
