04284824cb02cd8e074b8824a4e03c38d55864c9
Pawel Darulewski <pawel.darulewski@gmail.com>
2022-02-03 13:50:21 +0100

    feat: cnn model simplification

--- continual_learning/models/cnn.py
+++ continual_learning/models/cnn.py
@@ -1,19 +1,20 @@
-import pytorch_lightning as pl
 import torch
 import torch.nn.functional as F
 import torchmetrics
+from pytorch_lightning import LightningModule
 from torch import nn
 
 
-class CNN(pl.LightningModule):
+class CNN(LightningModule):
+
     def __init__(self):
         super().__init__()
-        self.conv1 = nn.Conv2d(1, 32, 3, 1)
-        self.conv2 = nn.Conv2d(32, 64, 3, 1)
+        self.conv1 = nn.Conv2d(1, 8, 3, 1)
+        self.conv2 = nn.Conv2d(8, 16, 3, 1)
         self.dropout1 = nn.Dropout(0.25)
         self.dropout2 = nn.Dropout(0.5)
-        self.fc1 = nn.Linear(9216, 128)
-        self.fc2 = nn.Linear(128, 10)
+        self.fc1 = nn.LazyLinear(16)
+        self.fc2 = nn.LazyLinear(10)
 
         acc = torchmetrics.Accuracy()
         self.train_acc = acc.clone()
