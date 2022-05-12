af4f05a2784cc6689c1e47ac81de2590def408f8
Pawel Darulewski <pawel.darulewski@gmail.com>
2022-05-12 10:23:14 +0200

    feat: map

--- continual_ranking/dpr/data/evaluator.py
+++ continual_ranking/dpr/data/evaluator.py
@@ -1,31 +1,12 @@
 from typing import Dict
 
-import numba
-import numba.cuda
+import numpy as np
 import torch
+import tqdm
 
 from continual_ranking.dpr.data.data_module import DataModule
 from continual_ranking.dpr.data.file_handler import pickle_load
 from continual_ranking.dpr.models.biencoder import dot_product
-
-
-@numba.jit(parallel=True)
-def _k_docs(scores, index_dataset, test_dataset):
-    full_k = [5] + list(range(10, 110, 10))
-    top_k_docs = {k: 0 for k in full_k}
-
-    for i in numba.prange(scores):
-        for k in full_k:
-            top_items = torch.topk(scores[i], k).indices
-            positive = test_dataset[i].context_ids[0]
-            for j in top_items:
-                if torch.equal(index_dataset[j].input_ids, positive):
-                    top_k_docs[k] += 1
-
-        if i % 100 == 0:
-            print(i)
-
-    return top_k_docs
 
 
 class Evaluator:
@@ -37,7 +18,7 @@ class Evaluator:
         self.index_path = index_path
         self.test_path = test_path
 
-        self.k = [5] + list(range(10, 110, 10))
+        self.k = [5, 10, 20, 50, 100]
         self.top_k_docs = {k: 0 for k in self.k}
 
         self.device = device
@@ -46,12 +27,46 @@ class Evaluator:
         test = pickle_load(self.test_path).to(self.device)
         index = pickle_load(self.index_path).to(self.device)
 
-        scores = dot_product(test, index).numpy()
+        self.scores = dot_product(test, index)
 
-        self.top_k_docs = _k_docs(scores, self.index_dataset, self.test_dataset)
+        self._k_docs()
+
+    def _k_docs(self):
+        index_data = [i for i in self.index_dataset]
+        for k in self.k:
+            top_items = torch.topk(self.scores, k).indices
+            top_items = top_items.numpy()
+
+            for i, sample in enumerate(tqdm.tqdm(top_items)):
+                positive = self.test_dataset[i].context_ids[0]
+
+                for j in sample:
+                    if torch.equal(self.index_dataset[j].input_ids, positive):
+                        self.top_k_docs[k] += 1
 
     def _calculate_acc(self) -> Dict[str, float]:
         return {f'k_acc_{key}': value / len(self.test_dataset) for key, value in self.top_k_docs.items()}
+
+    @staticmethod
+    def average_precision(actual, predicted, k=10):
+        if len(predicted) > k:
+            predicted = predicted[:k]
+
+        score = 0.0
+        num_hits = 0.0
+
+        for i, p in enumerate(predicted):
+            if p in actual and p not in predicted[:i]:
+                num_hits += 1.0
+                score += num_hits / (i + 1.0)
+
+        if not actual:
+            return 0.0
+
+        return score / min(len(actual), k)
+
+    def mean_average_precision(self, actual, predicted, k=10):
+        return np.mean([self.average_precision(a, p, k) for a, p in zip(actual, predicted)])
 
     def evaluate(self) -> Dict[str, float]:
         self._calculate_k_docs()
