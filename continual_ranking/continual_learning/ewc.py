fix: ewc gradients

continual_ranking/continual_learning/ewc.py
@@ -29,12 +29,12 @@ class EWC(Strategy):
             self.saved_params = {}
             for n, p in pl_module.named_parameters():
                 if p.requires_grad and p is not None:
-                    self.saved_params[n] = p.data.detach().clone()
+                    self.saved_params[n] = p.data
 
     def calculate_importances(self, trainer: ContinualTrainer, pl_module: BiEncoder, train_dataloader: DataLoader):
         self.fisher_matrix = {}
         for n, p in self.saved_params.items():
-            t = torch.zeros_like(p.data).detach()
+            t = torch.zeros_like(p.data)
             self.fisher_matrix[n] = t
 
         pl_module.ewc_mode = True
@@ -50,7 +50,7 @@ class EWC(Strategy):
         penalty = 0
         for n, p in pl_module.named_parameters():
             if n in self.fisher_matrix:
-                diff = (p - self.saved_params[n].to(pl_module.device)) ** 2
+                diff = (p - self.saved_params[n].to(pl_module.device)).pow(2)
                 loss = self.fisher_matrix[n].to(pl_module.device) * diff
                 penalty += loss.sum()
         return penalty

continual_ranking/dpr/models/biencoder.py
@@ -171,12 +171,11 @@ class BiEncoder(pl.LightningModule):
         with torch.enable_grad():
             self.zero_grad()
             ewc_loss, _, _ = self.shared_step(batch, batch_idx, False)
-        ewc_loss.backward()
+            ewc_loss.backward()
 
-        with torch.no_grad():
             for n, p in self.named_parameters():
                 if n in self.fisher_matrix and p.grad is not None:
-                    self.fisher_matrix[n].data += p.grad.data.to('cpu').detach().clone() ** 2
+                    self.fisher_matrix[n].data += p.grad.data.to('cpu').clone().pow(2)
 
         return ewc_loss
 